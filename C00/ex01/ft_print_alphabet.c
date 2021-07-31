4d76c7ccb10c306ba20d381dd7753c95899fb899
caiohmat <87320308+caiohmat@users.noreply.github.com>
2021-07-30 21:48:56 -0300

    Update ft_print_alphabet.c

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -11,15 +11,3 @@ void	ft_print_alphabet(void)
 		letter++;
 	}
 }
-
-void    ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
-
-int    main(void)
-{
-    ft_print_reverse_alphabet();
-    return (0);
-}
